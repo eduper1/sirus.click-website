5e0971d4668d168c31c94bf309fd29522a033872
eduper1 <eduper1@gmail.com>
2021-09-26 20:59:06 +0300

    ...

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
+// function myFunction() {
+//     var x = document.getElementById("myLinks");
+//     if (x.style.display === "block") {
+//         x.style.display = "none";
+//     } else {
+//         x.style.display = "block";
+//     }
+// }
+
+$("#Home").click(function() {
+    $("#home").show();
+    $("#about").hide();
+    //$("#donate").hide();
+});
+$("#About").click(function() {
+    $("#about").show();
+    $("#home").hide();
+    //$("#donate").hide();
+});
+// $("#Donate").click(function() {
+//     $("#donate").show();
+//     $("#about").hide();
+//     $("#home").hide();
+// });
--- template/js/data.js
+++ template/js/data.js
@@ -1,50 +1,15 @@
-// @ts-check
-
-//const nav = document.querySelectorAll(".bar");
-//console.log(nav);
-
-
-const gridCells = document.querySelectorAll(".bar");
-gridCells.forEach((bar) => {
-  // Set outline when cell is hovered
-  bar.addEventListener('click', click) ;
+$("#Home").click(function() {
+    $("#home").show();
+    $("#projects").hide();
+    $("#donate").hide();
 });
-
-function click(e){
-  e.preventDefault();
-  let main = document.querySelector('main');
-    // Navbar links
-  const home = document.getElementById('Home');
-  const project = document.getElementById('Projects');
-  const about = document.getElementById('About' );
-  const donate = document.getElementById('Donate');
-  // content to be displayed when nav link is clicked
-  const homeContent = document.querySelector('#home');
-  const proContent  = document.querySelector('#projects');
-  const aboutContent = document.querySelector('#about');
-  
-  home.addEventListener('click', () => {
-    console.log(2);
-    main.innerHTML = homeContent.innerHTML;
-    main.style.display = "block";
-    })
-  
-  project.addEventListener('click', () => {
-    console.log(3);
-    main.innerHTML = proContent.innerHTML;
-    main.style.display = "block";
-    })
-  
-  about.addEventListener('click', () => {
-    console.log(4);
-    main.innerHTML = aboutContent.innerHTML;
-    main.style.display = "block";
-    })
-
-  donate.addEventListener('click', () => {
-    console.log(5);
-    //main.innerHTML = homeContent.innerHTML;
-    //main.style.display = "block";
-    })
-}
-  
+$("#Projects").click(function() {
+    $("#projects").show();
+    $("#home").hide();
+    $("#donate").hide();
+});
+$("#Donate").click(function() {
+    $("#donate").show();
+    $("#projects").hide();
+    $("#home").hide();
+});
